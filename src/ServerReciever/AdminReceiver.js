dcc5518ab6988ee711372fc9a84e886c360ef853
Danila <dansil2301@gmail.com>
2024-05-13 13:18:01 +0200

    added admin endpoint to data access layer

---
+++ src/ServerReciever/AdminReceiver.js
@@ -0,0 +1,65 @@
+import MainServeURL from "../../config.js";
+import axios from "axios";
+import {TraderToken} from "../Logic/TraderLogic/TraderToken.js";
+
+export class AdminReceiver {
+    static GetTradersPages(params) {
+        const API_URL = MainServeURL + `api/traders/pages`;
+        return new Promise((resolve, reject) => {
+            axios.put(API_URL, params, {
+                headers: {'Authorization': `Bearer ${TraderToken.getToken()}`}
+            })
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+
+    static GetSearchedTradersPages(params) {
+        const API_URL = MainServeURL + `api/traders/search`;
+        return new Promise((resolve, reject) => {
+            axios.put(API_URL, params, {
+                headers: {'Authorization': `Bearer ${TraderToken.getToken()}`}
+            })
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+
+    static GetCountAllTraders() {
+        const API_URL = MainServeURL + `api/traders/countAllStatusTraders`;
+        return new Promise((resolve, reject) => {
+            axios.put(API_URL, {
+                headers: {'Authorization': `Bearer ${TraderToken.getToken()}`}
+            })
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+
+    static GetCountStatusTraders(params) {
+        const API_URL = MainServeURL + `api/traders/countAllTraders`;
+        return new Promise((resolve, reject) => {
+            axios.put(API_URL, params, {
+                headers: {'Authorization': `Bearer ${TraderToken.getToken()}`}
+            })
+                .then(response => {
+                    resolve(response.data);
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        });
+    }
+}
