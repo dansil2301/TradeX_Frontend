f165743a0e70e4e0beb8555b87596b42dca465c5
Danila Solovenko <dansil2301@gmail.com>
2024-05-22 20:20:30 +0200

    added statistics empty page

---
+++ src/Pages/StatisticsPage.jsx
@@ -0,0 +1,10 @@
+import {
+    TraderOptionsHeader
+} from "../Components/PagesComponents/TraderOptionsPage/TraderOptionsHeader/TraderOptionsHeader.jsx";
+export function StatisticsPage() {
+    return (
+        <div className="AccountBody" style={{height: 'calc(100vh - 35px)'}}>
+            <TraderOptionsHeader />
+        </div>
+    )
+}
